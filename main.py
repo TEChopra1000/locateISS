48d7e146c5d09fe1b3254c3009493842ea0e8e66
TEChopra1000 <talia.chopra@gmail.com>
2019-01-02 22:22:37 -0800

    added map using turtle, and marker to indicate lat. and long. of iss

--- main.py
+++ main.py
@@ -26,3 +26,27 @@ for p in people:
 	print(p['name'])
 
 #####Getting the Location of the ISS#####
+
+url = 'http://api.open-notify.org/iss-now.json'
+response = urllib.request.urlopen(url)
+result = json.loads(response.read())
+
+location = result['iss_position']
+lat = float(location['latitude'])
+lon = float(location['longitude'])
+print("longtitude", lon, "\n", "latitude", lat, "\n")
+
+#####Creating a Map to Display Location#####
+
+screen = turtle.Screen()
+screen.setup(720, 360)
+screen.setworldcoordinates(-180,-90,180,90)
+screen.bgpic('map.gif')
+
+screen.register_shape('iss.gif')
+iss = turtle.Turtle()
+iss.shape('iss.gif')
+iss.setheading(90)
+
+iss.penup()
+iss.goto(lon, lat)
